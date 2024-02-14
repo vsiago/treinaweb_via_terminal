2cfc34d589dc0d098b8d000eeda05e2a63958d4e
vsiago <vsiago22@gmail.com>
2024-02-14 11:56:43 -0300

    Curso Flutter Treinaweb

---
+++ android/app/src/main/kotlin/com/example/treinaweb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.treinaweb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(const Sorteador());
+}
+
+class Sorteador extends StatelessWidget {
+  const Sorteador({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Text('Ola mundo! Treinaweb!!'));
+  }
+}
